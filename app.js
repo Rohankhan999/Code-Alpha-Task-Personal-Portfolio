5e1e7a2dda522318a0de2029f1f6579eacb39cb0
Rohankhan999 <rohankhan08453@gmail.com>
2024-12-20 20:39:10 +0500

    .

---
+++ app.js
@@ -0,0 +1,15 @@
+function aboutme(){
+        document.getElementById("about").scrollIntoView({ behavior: "smooth" });  
+}
+function Home(){
+    document.getElementById("Home").scrollIntoView({ behavior: "smooth" });  
+}
+function Skills(){
+    document.getElementById("Skills").scrollIntoView({ behavior: "smooth" });  
+}
+function project(){
+    document.getElementById("project").scrollIntoView({ behavior: "smooth" });  
+}
+function Contact(){
+    document.getElementById("Contact").scrollIntoView({ behavior: "smooth" });  
+}
